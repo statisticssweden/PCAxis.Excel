713fa46dbaa9e2b96d2f5db5999f26dd4815760a
Kent Mossbäck <kent.mossback@scb.se>
2020-09-14 17:05:56 +0200

    Write UnitTest

--- PCAxis.Excel.UnitTest/XlsxSerializerTest.cs
+++ PCAxis.Excel.UnitTest/XlsxSerializerTest.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCAxis.Paxiom;
 
 namespace PCAxis.Excel.UnitTest
 {
@@ -20,5 +24,36 @@ namespace PCAxis.Excel.UnitTest
 
 
 		}
+
+        [TestMethod]
+        public void ShouldSerialize()
+        {
+            var pxData = new PXData();
+			//pxData.
+
+   //         pxData.CurrentIndex = 2;
+		
+   //         pxData.Matrix = 2,2;
+
+			
+            var pxMeta = new PXMeta();
+
+			var pxModel = new PXModel();
+
+            //pxModel.Data = pxData;
+
+			var ser = new XlsxSerializer();
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("whatever"));
+
+        //	var stream = 
+
+            ser.Serialize(pxModel, stream);
+
+			Assert.AreEqual(1,1); 
+
+        }
+
+
 	}
 }
